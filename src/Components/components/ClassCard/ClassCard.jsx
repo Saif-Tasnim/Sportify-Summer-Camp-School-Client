5fbdb7a4097ce68b0a29472f75fdc9c267b94010
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-07-25 09:42:54 +0600

    card class designed

--- src/Components/components/ClassCard/ClassCard.jsx
+++ src/Components/components/ClassCard/ClassCard.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
-const ClassCard = () => {
+const ClassCard = ({ c }) => {
+    console.log(c);
+
     return (
-        <div>
-            <h1> This is class card </h1>
+
+        <div className="card w-96 bg-base-100 shadow-xl image-full">
+            <figure><img src={c.image} alt="images" /></figure>
+            <div className="card-body">
+                <h2 className="card-title pb-4 pt-4"> {c.className} </h2>
+                <div className='text-center text-orange-300'>
+                    <h4 className='pb-2'> Status : {c.status} </h4>
+                    <h4 className='pb-2'>Total Seats : {c.seats} </h4>
+                    <h4 className='pb-2'>Enrolled Students : {c.enrolled ? c.enrolled : 0}</h4>
+                </div>
+                {
+                    c.feedback && <div>
+                        Feedback :
+                    </div>
+                }
+                <div className="card-actions justify-end mt-5">
+                    <button className="btn btn-primary"> Update Class </button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Instructor/MyClass/MyClass.jsx
+++ src/Pages/Instructor/MyClass/MyClass.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from '../../../Providers/AuthProviders';
 import ClassCard from '../../../Components/components/ClassCard/ClassCard';
 
 const MyClass = () => {
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
     const [classList, refetch] = useClass();
 
     return (
@@ -12,10 +12,15 @@ const MyClass = () => {
             <h1 className='pt-10 mb-16 text-center text-3xl text-[#798132] underline'> All Class Of {user?.displayName} </h1>
 
             {/* class card  */}
-            {
-                classList.map (c=> <ClassCard></ClassCard>)
-            }
+            <div className='w-3/4 mx-auto grid grid-cols-3 gap-10'>
+                {
+                    classList.map(c => <ClassCard
+                        key={c._id}
+                        c={c}
+                    ></ClassCard>)
+                }
 
+            </div>
         </div>
     );
 };

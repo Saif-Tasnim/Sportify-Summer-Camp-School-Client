ed6c49859f50582acf63e50336ee5fe290c28361
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-07-27 10:38:08 +0600

    table design has updated

--- src/Pages/Admin/ManageClass/ManageClass.jsx
+++ src/Pages/Admin/ManageClass/ManageClass.jsx
@@ -5,19 +5,19 @@ import { AuthContext } from '../../../Providers/AuthProviders';
 
 const ManageClass = () => {
     const [axiosSecure] = useAxiosSecure();
-    const {user, loading} = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
 
-    const {data: classData=[], refetch } = useQuery({
+    const { data: classData = [], refetch } = useQuery({
         queryKey: ['ManageClass'],
         enabled: !loading,
-        queryFn: async ()=>{
-           const res = await axiosSecure.get('/class/admin/manage');
-           return res.data;
+        queryFn: async () => {
+            const res = await axiosSecure.get('/class/admin/manage');
+            return res.data;
         }
     })
 
     console.log(classData);
-     
+
     return (
         <div className='pt-20 bg-base-200'>
             <h1 className='pt-10 mb-16 text-center text-3xl text-[#798132] underline'> All Pending Classes List  </h1>
@@ -29,26 +29,40 @@ const ManageClass = () => {
                     <thead className='text-base font-bold bg-slate-600 text-[#EEFF25] ml-4'>
                         <tr>
                             <th>  #  </th>
+                            <th> Iamge </th>
                             <th> Class Name </th>
                             <th> Instructor  </th>
                             <th> Instructor Email </th>
+                            <th> Available Seats </th>
+                            <th> Price </th>
                             <th> Status </th>
-                            <th> Action </th>
                             <th> Action </th>
                             <th> Action </th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            classData.map((d,index) => <tr>
-                            <td> {index+1} </td>
-                            <td> {d.className} </td>
-                            <td> {d.name} </td>
-                            <td> {d.email} </td>
-                            <td> {d.status} </td>
-                            <td> <button>Accept</button></td>
-                            <td> <button> Denied </button></td>
-                            <td> <button>Feedback</button> </td>
+                            classData.map((d, index) => <tr>
+                                <td> {index + 1} </td>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={d.image} />
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </td>
+                                <td> {d.className} </td>
+                                <td> {d.name} </td>
+                                <td> {d.email} </td>
+                                <td className='text-center'> {d.seats} </td>
+                                <td> ${d.price} </td>
+                                <td> {d.status} </td>
+                                <td> <button className="btn btn-outline btn-success">Accept</button></td>
+                                
+                                <td><button className="btn btn-outline btn-error"> Deny </button></td>
 
                             </tr>)
                         }
